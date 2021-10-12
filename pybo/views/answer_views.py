adb0a6b15671f1015338b3acaa73bf80fb29c239
김윤태 <qndbsxo@naver.com>
2021-10-12 10:00:58 +0900

    3-13 스크롤 초기화 문제점 해결하기

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -26,45 +26,48 @@ def create(question_id):
     question = Question.query.get_or_404(question_id)
     if form.validate_on_submit():
         content = request.form["content"]
-        answer = Answer(
-            content=content,
-            create_date=datetime.now(),
-            user=g.user
-        )
+        answer = Answer(content=content, create_date=datetime.now(), user=g.user)
         question.answer_set.append(answer)
         db.session.commit()
-        return redirect(url_for("question.detail", question_id=question_id))
+        return redirect(
+            "{}#answer_{}".format(
+                url_for("question.detail", question_id=question_id), answer.id
+            )
+        )
     return render_template(
         "question/question_detail.html", question=question, form=form
     )
 
-@bp.route('/modify/<int:answer_id>', methods=['GET', 'POST'])
+
+@bp.route("/modify/<int:answer_id>", methods=["GET", "POST"])
 @login_required
 def modify(answer_id):
     answer = Answer.query.get_or_404(answer_id)
     if g.user != answer.user:
-        flash('수정권한이 없습니다')
-        return redirect(url_for('question.detail', question_id=answer.question.id))
-    if request.method == 'POST':
+        flash("수정권한이 없습니다")
+        return redirect(url_for("question.detail", question_id=answer.question.id))
+    if request.method == "POST":
         form = AnswerForm()
         if form.validate_on_submit():
             form.populate_obj(answer)
-            answer.modify_date = datetime.now() # 수정일시 저장
+            answer.modify_date = datetime.now()  # 수정일시 저장
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=answer.question.id))
+            return redirect('{}#answer_{}'.format(
+                url_for('question.detail', question_id=answer.question.id), answer_id
+            ))
     else:
         form = AnswerForm(obj=answer)
-    return render_template('answer/answer_form.html', form=form)
+    return render_template("answer/answer_form.html", form=form)
 
-@bp.route('/delete/<int:answer_id>')
+
+@bp.route("/delete/<int:answer_id>")
 @login_required
 def delete(answer_id):
     answer = Answer.query.get_or_404(answer_id)
     question_id = answer.question.id
     if g.user != answer.user:
-        flash('삭제권한이 없습니다')
+        flash("삭제권한이 없습니다")
     else:
         db.session.delete(answer)
         db.session.commit()
-    return redirect(url_for('question.detail', question_id=question_id))
-
+    return redirect(url_for("question.detail", question_id=question_id))
--- pybo/views/comment_views.py
+++ pybo/views/comment_views.py
@@ -20,7 +20,9 @@ def create_question(question_id):
         comment = Comment(user=g.user, content=form.content.data, create_date=datetime.now(), question=question)
         db.session.add(comment)
         db.session.commit()
-        return redirect(url_for('question.detail', question_id=question_id))
+        return redirect('{}#comment_{}'.format(
+            url_for('question.detail', question_id=question_id), comment.id
+        ))  
     return render_template('comment/comment_form.html', form=form)
 
 
@@ -37,7 +39,9 @@ def modify_question(comment_id):
             form.populate_obj(comment)
             comment.modify_date = datetime.now()  # 수정일시 저장
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=comment.question.id))
+            return redirect('{}comment_{}'.format(
+                url_for('question.detail', question_id=comment.question.id), comment.id
+            ))
     else:
         form = CommentForm(obj=comment)
     return render_template('comment/comment_form.html', form=form)
@@ -65,7 +69,9 @@ def create_answer(answer_id):
         comment = Comment(user=g.user, content=form.content.data, create_date=datetime.now(), answer=answer)
         db.session.add(comment)
         db.session.commit()
-        return redirect(url_for('question.detail', question_id=answer.question.id))
+        return redirect('{}#comment_{}'.format(
+            url_for('question.detail', question_id=answer.question.id), comment.id
+        ))
     return render_template('comment/comment_form.html', form=form)
 
 
@@ -82,7 +88,9 @@ def modify_answer(comment_id):
             form.populate_obj(comment)
             comment.modify_date = datetime.now()  # 수정일시 저장
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=comment.answer.question.id))
+            return redirect('{}#comment_{}'.format(
+                url_for('question.detail', question_id=comment.answer.question.id), comment.id
+            ))
     else:
         form = CommentForm(obj=comment)
     return render_template('comment/comment_form.html', form=form)
@@ -95,7 +103,7 @@ def delete_answer(comment_id):
     question_id = comment.answer.question.id
     if g.user != comment.user:
         flash('삭제권한이 없습니다')
-        return redirect(url_for('question.detail', question_id=question_id))
+        return redirect
     db.session.delete(comment)
     db.session.commit()
     return redirect(url_for('question.detail', question_id=question_id))
