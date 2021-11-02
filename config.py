ba2ae510692b2663d75e92deaeec9cd76cc6d588
김윤태 <qndbsxo@naver.com>
2021-11-02 13:13:50 +0900

    db서버 변경

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-import os 
+import os
 
 BASE_DIR = os.path.dirname(__file__)
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{pw}@{url}/{db}'.format(
-	user='root',
-	pw="5257",
-	url='localhost',
-	db='mydatabase'		
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{user}:{pw}@{url}/{db}".format(
+    user="admin",
+    pw="wasdas5257",
+    url="database-2.cten155hsop6.ap-northeast-2.rds.amazonaws.com",
+    db="mydatabase",
 )
-SQLALCHEMY_TRACK_MODIFICATIONS= False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "dev"
